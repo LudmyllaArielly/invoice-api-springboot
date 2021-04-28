e7664925345b63019d93aa9f4566c489bf8a1ca7
Ludmylla Arielly <ludmay4@gmail.com>
2021-04-28 10:52:43 -0300

    create new user dto

---
+++ src/main/java/com/ludmylla/invoice/model/dto/UseCreateAndListAllDTO.java
@@ -0,0 +1,46 @@
+package com.ludmylla.invoice.model.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class UseCreateAndListAllDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String firstName;
+	private String lastName;
+	private String cpf;
+	private Date dateOfBirth;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+}
