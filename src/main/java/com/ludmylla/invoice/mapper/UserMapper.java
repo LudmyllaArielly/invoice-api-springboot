c817e94bf2c0b17e19fa5f9dac3d048e9cceb1f1
Ludmylla Arielly <ludmay4@gmail.com>
2021-04-28 09:57:43 -0300

    adding user mapper

---
+++ src/main/java/com/ludmylla/invoice/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.ludmylla.invoice.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+import com.ludmylla.invoice.model.User;
+import com.ludmylla.invoice.model.dto.UserCpfDTO;
+
+@Mapper
+public interface UserMapper {
+	
+	UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+	
+	@Mappings({
+		@Mapping(target="id", ignore = true),
+		@Mapping(target="firstName", ignore = true),
+		@Mapping(target="lastName", ignore = true),
+		@Mapping(target="dateOfBirth", ignore = true),
+	})
+	User toUser (UserCpfDTO source);
+}
