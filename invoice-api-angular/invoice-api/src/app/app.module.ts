ce4f7f4da181819e90f0223aec82a4899c30272a
Ludmylla Arielly <ludmay4@gmail.com>
2021-04-30 14:12:56 -0300

    remove angular/material

--- invoice-api-angular/invoice-api/src/app/app.module.ts
+++ invoice-api-angular/invoice-api/src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './pages/components/invoice/list/list.component';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { ListModule } from './pages/components/invoice/list/list.module';
 
@@ -18,8 +16,6 @@ import { ListModule } from './pages/components/invoice/list/list.module';
   imports: [
     BrowserModule,
     NoopAnimationsModule,
-    MatSliderModule,
-    MatTableModule,
     AppRoutingModule,
     ListModule
   ],
--- invoice-api-angular/invoice-api/src/app/pages/components/invoice/list/list.module.ts
+++ invoice-api-angular/invoice-api/src/app/pages/components/invoice/list/list.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ListRoutingModule } from './list-routing.module';
-import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ListRoutingModule,
-    MatTableModule
+    ListRoutingModule
+
   ]
 })
 export class ListModule { }
