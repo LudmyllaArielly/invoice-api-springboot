5c3c4f64fc250984448e06b9bea5aacb2bacaf83
Ludmylla Arielly <ludmay4@gmail.com>
2021-04-30 10:30:41 -0300

    added end-point update invoice

--- src/main/java/com/ludmylla/invoice/resource/InvoiceResource.java
+++ src/main/java/com/ludmylla/invoice/resource/InvoiceResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import com.ludmylla.invoice.mapper.InvoiceMapper;
 import com.ludmylla.invoice.model.Invoice;
 import com.ludmylla.invoice.model.dto.InvoiceCreateAndListAllDTO;
 import com.ludmylla.invoice.model.dto.InvoiceListAllDTO;
+import com.ludmylla.invoice.model.dto.InvoiceUpdateDTO;
 import com.ludmylla.invoice.service.InvoiceService;
 
 
@@ -62,6 +64,17 @@ public class InvoiceResource {
 		}
 	}
 	
+	@PutMapping
+	public ResponseEntity<String> updatedInvoice(@RequestBody InvoiceUpdateDTO invoiceUpdateDTO){
+		try {
+			
+			Invoice invoice = InvoiceMapper.INSTANCE.toInvoice(invoiceUpdateDTO);
+			invoiceService.updateInvoice(invoice);
+			return ResponseEntity.status(HttpStatus.OK).body("Invoice updated successfully! ");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to update invoice: " + e.getMessage());
+		}
+	}
 	
 	
 }
