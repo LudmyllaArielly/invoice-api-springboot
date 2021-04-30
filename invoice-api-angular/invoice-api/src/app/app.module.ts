1483c0fa94eb65d4f688fb67dcd4283685435351
Ludmylla Arielly <ludmay4@gmail.com>
2021-04-30 12:21:35 -0300

    initial commit invoice-api-angular

---
+++ invoice-api-angular/invoice-api/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GetAllInvoiceComponent } from './pages/components/get-all-invoice/get-all-invoice.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GetAllInvoiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ invoice-api-angular/invoice-api/src/app/pages/components/get-all-invoice/get-all-invoice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-all-invoice',
+  templateUrl: './get-all-invoice.component.html',
+  styleUrls: ['./get-all-invoice.component.scss']
+})
+export class GetAllInvoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
