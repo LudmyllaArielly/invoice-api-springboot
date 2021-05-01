9849d006f6de85ec6b0309d6580c3801915a1f3c
Ludmylla Arielly <ludmay4@gmail.com>
2021-05-01 17:27:36 -0300

    added example card in the html details and routing

--- invoice-api-angular/invoice-api/src/app/pages/components/invoice/details/details-routing.module.ts
+++ invoice-api-angular/invoice-api/src/app/pages/components/invoice/details/details-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'details/:id', component: DetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
