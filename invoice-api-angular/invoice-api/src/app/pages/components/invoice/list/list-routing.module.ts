a0a6ac22de23b4e39ae63fa6e7f27270d724800a
Ludmylla Arielly <ludmay4@gmail.com>
2021-05-04 15:41:47 -0300

    redirecting main path to invoice list

--- invoice-api-angular/invoice-api/src/app/pages/components/invoice/list/list-routing.module.ts
+++ invoice-api-angular/invoice-api/src/app/pages/components/invoice/list/list-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list.component';
 
 
-const routes: Routes = [{ path: 'list', component: ListComponent }];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
