abc3fb6dd9735c79860310e6a3dd0bb973907607
Ludmylla Arielly <ludmay4@gmail.com>
2021-06-07 15:04:30 -0300

    configure cors and system properties

--- invoice-api/src/main/java/com/ludmylla/invoice/InvoiceApiApplication.java
+++ invoice-api/src/main/java/com/ludmylla/invoice/InvoiceApiApplication.java
@@ -1,11 +1,16 @@
 package com.ludmylla.invoice;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class InvoiceApiApplication {
@@ -28,6 +33,15 @@ public class InvoiceApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceApiApplication.class, args);
+	}
+	
+	@Bean
+	CorsConfigurationSource configurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
