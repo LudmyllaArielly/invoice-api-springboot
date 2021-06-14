91df30f26ba57e7abd79fe7ac14380edb331ed4e
Ludmylla Arielly <ludmay4@gmail.com>
2021-06-14 19:06:15 -0300

    create new user

--- invoice-app/src/app/pages/components/user/new-user/new-user.component.ts
+++ invoice-app/src/app/pages/components/user/new-user/new-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NewUser } from 'src/app/shared/model/user/new-user.model';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-new-user',
@@ -15,14 +17,25 @@ export class NewUserComponent implements OnInit {
     lastName: ''
   }
 
-  constructor() { }
+  constructor(private userService: UserService, private route: Router) { }
 
   ngOnInit(): void {
   }
 
+  saveUser(){
+    this.userService.newUser(this.newUser).subscribe(data => {
+      console.log(data);
+      this.goToUserList();
+    }, error => console.log(error));
+  }
+
+  goToUserList(){
+    this.route.navigate(['/listUser']);
+  }
 
   onSubmit() {
     console.log(this.newUser);
+    this.saveUser();
   }
 
 }
