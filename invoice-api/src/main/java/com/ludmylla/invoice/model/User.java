01c61f73006d6b070fd07662f6c421d63fe60197
Ludmylla Arielly <ludmay4@gmail.com>
2021-06-15 18:21:59 -0300

    alter database and config application properties

--- invoice-api/src/main/java/com/ludmylla/invoice/model/User.java
+++ invoice-api/src/main/java/com/ludmylla/invoice/model/User.java
@@ -8,12 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
