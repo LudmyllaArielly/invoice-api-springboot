6fc6455ab4c775cb6a9c18d39d07baf6ff554b6b
Ludmylla Arielly <ludmay4@gmail.com>
2021-06-15 17:17:35 -0300

    create handler for exceptions

--- invoice-app/src/app/shared/user.service.ts
+++ invoice-app/src/app/shared/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { ListAllUser } from './model/user/list-user.model';
 import { NewUser } from './model/user/new-user.model';
 
@@ -14,18 +15,47 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getAllUsers(): Observable<ListAllUser[]> {
-    return this.http.get<ListAllUser[]>(this.baseUrl);
+    return this.http.get<ListAllUser[]>(this.baseUrl)
+    .pipe (
+      retry(1),
+      catchError(this.handlerError)
+    );
   }
 
   newUser(newUser: NewUser): Observable<Object> {
-    return this.http.post(this.baseUrl, newUser, { responseType: 'text' });
+    return this.http.post(this.baseUrl, newUser, { responseType: 'text' })
+    .pipe (
+      retry(1),
+      catchError(this.handlerError)
+    );
   }
 
   getUserFindById(id: number): Observable<NewUser> {
-    return this.http.get<NewUser>(`${this.baseUrl}/${id}`);
+    return this.http.get<NewUser>(`${this.baseUrl}/${id}`)
+    .pipe (
+      retry(1),
+      catchError(this.handlerError)
+    );
   }
 
   updateUser(newUser: NewUser): Observable<Object>{
-    return this.http.put(this.baseUrl, newUser,{ responseType: 'text' });
+    return this.http.put(this.baseUrl, newUser,{ responseType: 'text' })
+    .pipe (
+      retry(1),
+      catchError(this.handlerError)
+    );
+  }
+
+  // Error handling
+  handlerError(error) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent){
+      // Get client side error
+      errorMessage = error.error.message;
+    }else {
+      errorMessage = `Error code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 }
